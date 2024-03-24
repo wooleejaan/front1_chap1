69ef66f1a1cd1adbc0056ca169df1e88ab6f1fda
yongwoo <wooleejaan@gmail.com>
2024-03-24 22:57:52 +0900

    feat: Implemented assignment F

--- packages/chapter2/src/observer/store.js
+++ packages/chapter2/src/observer/store.js
@@ -1,6 +1,20 @@
+import { 발행기관, 구독 } from "./pubsub";
+
 export class Store {
-  constructor({ state, mutations, actions }) {}
+  constructor({ state, mutations, actions }) {
+    this.state = 발행기관(state);
+    this.mutations = mutations;
+    this.actions = actions;
+  }
 
-  commit(action, payload) {}
-
+  commit(action, payload) {
+    if (!typeof this.mutations[action]) {
+      console.error(`Action '${action}' is not defined in mutations.`);
+    }
+    try {
+      this.mutations[action](this.state, payload);
+    } catch (error) {
+      console.error(`Error executing action '${action}': ${error}`);
+    }
+  }
 }
